e9e5119c0d92bbb1f0477958efd667a57dbbfbf4
Jesse Aldridge <jessealdridge@gmail.com>
2014-06-15 19:17:12 -0700

    move new user into class so it can be overridden

--- simple_login.py
+++ simple_login.py
@@ -5,7 +5,40 @@ from flask import request, session
 
 class g:
     user_db = None
+    login_manager = None
 
+class LoginManager:
+
+    # Validate credentials and save a new user.
+
+    def new_user(self):
+        username, email, password = (
+            request.form['username'], request.form['email'],
+            request.form['password'])
+        email = email.lower()
+
+        if len(email) > 254 or not re.match('\w+@\w+\.\w+', email):
+            return 'bad email', 400
+        if len(username) > 30:
+            return 'username too long', 400
+        if not re.match('[a-zA-Z0-9_\-]+', username):
+            return 'illegal character in username', 400
+        if len(password) > 100:
+            return 'password too long', 400
+        if username in g.user_db['user_info']:
+            return 'user already exists', 400
+        if email in g.user_db['salts']:
+            return 'email already exists', 400
+
+        g.user_db['salts'][username] = salt = str(uuid.uuid4())
+        g.user_db['hashes'][username] = hashlib.sha224(password + salt).hexdigest()
+        g.user_db['user_info'][username] = {'email':email}
+
+        with open(g.user_db_path, 'w') as f:
+            f.write(json.dumps(g.user_db, indent=2))
+
+        session['username'] = username
+        return 'ok'
 
 # A decorator to require login.  Optionally redirect to a login page.
 
@@ -21,38 +54,6 @@ def require_login(redirect=False):
             return fn(*a, **kw)
         return decorated_function
     return decorator
-
-
-# Validate credentials and save a new user.
-
-def new_user():
-    username, email, password = (
-        request.form['username'], request.form['email'],
-        request.form['password'])
-    email = email.lower()
-
-    if len(email) > 254 or not re.match('\w+@\w+\.\w+', email):
-        return 'bad email', 400
-    if len(username) > 30:
-        return 'username too long', 400
-    if not re.match('[a-zA-Z0-9_\-]+', username):
-        return 'illegal character in username', 400
-    if len(password) > 100:
-        return 'password too long', 400
-    if username in g.user_db['user_info']:
-        return 'user already exists', 400
-    if email in g.user_db['salts']:
-        return 'email already exists', 400
-        
-    g.user_db['salts'][username] = salt = str(uuid.uuid4())
-    g.user_db['hashes'][username] = hashlib.sha224(password + salt).hexdigest()
-    g.user_db['user_info'][username] = {'email':email}
-
-    with open(g.user_db_path, 'w') as f:
-        f.write(json.dumps(g.user_db, indent=2))
-
-    session['username'] = username
-    return 'ok'
 
 
 # Verify username and password; log the user in.
@@ -78,10 +79,13 @@ def logout():
 
 # Add login related routes.  Load user credentials from json file.
 
-def init(app, user_db_path='user_db.json'):
+def init(app, user_db_path='user_db.json', login_manager=None):
+    g.login_manager = login_manager or LoginManager()
+
     app.add_url_rule('/logout', None, logout)
     app.add_url_rule('/login', None, login, methods=['GET', 'POST'])
-    app.add_url_rule('/new_user', None, new_user, methods=['POST'])
+    app.add_url_rule(
+        '/new_user', None, g.login_manager.new_user, methods=['POST'])
 
     g.user_db = {'hashes':{}, 'salts':{}, 'user_info':{}}
     g.user_db_path = user_db_path
@@ -95,7 +99,14 @@ if __name__ == '__main__':
     # Run the example app.
 
     app = flask.Flask(__name__)
-    init(app)
+
+    class CustomLogin(LoginManager):
+        def new_user(self):
+            response = LoginManager.new_user(self)
+            print 'new user created: ', session['username']
+            return response
+
+    init(app, login_manager=CustomLogin())
 
     # Generate a secret key like so:  import os; os.urandom(17)
     app.secret_key = 'a unique secret string used to encrypt sessions'
